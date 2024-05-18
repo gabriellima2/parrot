12b6e5404fb1044ebb50a40d00ac7a7b93fc0b00
Gabriel Lima <gabriel.rslima10@gmail.com>
2024-05-18 14:08:20 -0300

    first commit

---
+++ src/constants/metadata.ts
@@ -0,0 +1,10 @@
+export const METADATA = {
+	site: process.env.NEXT_PUBLIC_LOCAL_URL || '',
+	title: 'Banana',
+	description: '',
+	keywords: [],
+	applicationName: 'Banana',
+	authors: [{ name: 'Banana', url: process.env.NEXT_PUBLIC_LOCAL_URL || '' }],
+	creator: 'Banana',
+	publisher: 'Banana',
+}
